e2f76891039e98eedaaafb73f3daaa3922d97f83
apriliantoPutra <apriliantoputra696@gmail.com>
2025-05-06 16:31:13 +0700

    Terbaru Website

--- web_ta/routes/web.php
+++ web_ta/routes/web.php
@@ -4,11 +4,10 @@ use App\Http\Controllers\AkunController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Edukasi\ArtikelController;
 use App\Http\Controllers\Edukasi\VideoController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::get('/', [LoginController::class, 'index']);
 
 Route::get('/dashboard', [DashboardController::class, 'index', ]);
 Route::get('/akun', [AkunController::class, 'index', ]);
